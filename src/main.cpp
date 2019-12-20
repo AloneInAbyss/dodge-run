83a1d1ddee1f5cea116082f02584baba34afc1de
Thiago Assi <thiago-assi@outlook.com>
2019-12-20 17:19:36 -0300

    arquivos iniciais do platformio

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+
+/*
+ *    HEISENBERG - Dodge Run
+ *    Terceira Versão
+ *    Desenvolvedores: Thiago Assi & Michell Christofferson
+*/
+
+void setup() {
+
+}
+
+void loop() {
+
+}
+
+/*
+ *  IDEIAS PARA ADICIONAR
+ *  - Aprimorar a dificuldade progressiva
+ *  
+ *  - Menu, loja e modos de jogo (campanha/infinito/insano)
+ *  
+ *  - Remover redundância de limpar a tela e exibir tudo de novo a cada rodada,
+ *  basta apagar a antiga posição e exibir na nova
+ *  
+ *  - 'Troféus' a cada 100 de pontuação
+ *  
+ *  - Pontuação limite que muda o cenário do jogo (fim do jogo)
+ *  
+ *  - Adicionar tempo após Game Over para voltar a tela inicial
+ *  
+ *  - Melhorar função Random()
+ */
+
+ /*
+  * CHANGELOG
+  *   
+  */
